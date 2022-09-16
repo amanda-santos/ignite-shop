c81eb2c6b853a271983a8e329ca0e222d03bfeae
amanda-santos <amandasf0200@gmail.com>
2022-09-16 20:06:39 -0300

    feat: 💄 add carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/future/image";
+import { useKeenSlider } from "keen-slider/react";
 
 import { Container, Product } from "../styles/pages/home";
 
@@ -7,10 +8,19 @@ import tshirt1 from "../assets/t-shirts/1.png";
 import tshirt2 from "../assets/t-shirts/2.png";
 import tshirt3 from "../assets/t-shirts/3.png";
 
+import "keen-slider/keen-slider.min.css";
+
 const Home: NextPage = () => {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <Container>
-      <Product>
+    <Container ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={tshirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -19,7 +29,25 @@ const Home: NextPage = () => {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={tshirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>TShirt X</strong>
+          <span>$ 179.90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>TShirt X</strong>
+          <span>$ 179.90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image src={tshirt2} width={520} height={480} alt="" />
 
         <footer>
--- src/styles/pages/home/Product.ts
+++ src/styles/pages/home/Product.ts
@@ -3,7 +3,6 @@ import { styled } from "../..";
 export const Product = styled("a", {
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
   overflow: "hidden",

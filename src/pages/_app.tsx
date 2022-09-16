ba05f0d38267143c0ae05aa4575eeb761f4c0315
amanda-santos <amandasf0200@gmail.com>
2022-09-15 21:29:56 -0300

    feat: 💄 add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,32 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
